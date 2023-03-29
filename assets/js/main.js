3e086d2dd3e1a8353fe1f65ecdf4fbae60ceeb93
OksanaKarpyn <93349703+OksanaKarpyn@users.noreply.github.com>
2023-03-29 22:01:42 +0200

    add togle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,38 @@
-let arrayLista = ['Frutta', 'latte', 'pane', 'biscotti', 'caffe', 'verdure'];
+let arraySpesa = ['Frutta', 'latte', 'pane', 'biscotti', 'caffe', 'verdure'];
 let i = 0;
 let lista = document.querySelector('.lista-spesa');
 
 
-let btn = document.querySelector('.btn').addEventListener('click', function () {
+
+
+function generaList(arrayLista) {
+    let listUl = document.createElement('ul');
+    listUl.classList.add('list-group');
+    listUl.classList.add('list-group-flush');
+    listUl.classList.add('d-flex');
+    listUl.classList.add('align-middle')
+    listUl.classList.add('d-none');
+    lista.appendChild(listUl);
 
     while (i <= arrayLista.length - 1) {
-
-        let listUl = document.createElement('ul');
-        listUl.classList.add('list-group');
-        listUl.classList.add('list-group-flush');
-        listUl.classList.add('d-flex');
-        listUl.classList.add('align-middle')
-
-        lista.appendChild(listUl);
-
         let listItem = document.createElement('li');
-        listItem.innerHTML += `<li>${arrayLista[i]}</li>`;
+        listItem.innerHTML += `<span>${arrayLista[i]}</span>`;
         listItem.classList.add('list-group-item');
-        listItem.style.direction = 'rtl';
+        // listItem.style.direction = 'rtl';
         listUl.appendChild(listItem);
-
 
         let input = document.createElement('input');
         input.setAttribute('type', 'checkbox');
         input.style.margin = '5px';
-        listItem.appendChild(input);
+        listItem.prepend(input);
+       
+
         i++;
     }
+}
 
+
+let btn = document.querySelector('.btn').addEventListener('click', function () {
+lista.querySelector('ul').classList.toggle('d-none');
 });
+generaList(arraySpesa);
